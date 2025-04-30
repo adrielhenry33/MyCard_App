d055cfeaf2c9947c9aed6cb134029fc267c00d29
adrielhenry33 <adrielhenrydb@gmail.com>
2025-04-29 21:32:36 -0300

    Como utilizar um container com um unico filho

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,23 @@ void main() {
 }
 
 class MyWidget extends StatelessWidget {
-  //const MyWidget({super.key});
+  const MyWidget({super.key}); // construtor da classe MyWidget
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
 			home: Scaffold(
-				backgroundColor: Colors.blue,
-				body: Container(),
+				backgroundColor: Colors.green,
+				body: SafeArea(
+					child: Container(
+						padding: EdgeInsets.all(20.0),
+						margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
+						height: 100.0,
+						width: 100.0,
+						color: Colors.white,
+						child: Text(" Ola mundo "),
+					),
+				),
 			),
 		);
   }
